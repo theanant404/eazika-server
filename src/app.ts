24b81dbd61cbe85d81e7bbd25a7e2d55eed69593
Ashutosh Gaurav <mr.gaurav9554@gmail.com>
2025-11-23 11:54:12 +0530

    refactor: enhance swagger integration and improve development environment logging

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,8 @@ import express, { Application } from "express";
 import rateLimit from "express-rate-limit";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import env from "./config/env.config";
-import { swaggerUi, swaggerSpec } from "./swagger";
 
+import env from "./config/env.config";
 const { cors_origin, isNodeEnvDevelopment } = env;
 
 const app: Application = express();
@@ -25,16 +24,24 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+// if (isNodeEnvDevelopment) {
+//   app.use((req, _, next) => {
+//     console.log(
+//       `[Devlopment Environment] method: ${req.method}, url: ${req.protocol}://${req.get("host")}${req.originalUrl}, Time: ${new Date().toLocaleTimeString()}`
+//     );
+//     next();
+//   });
+// }
+
 app.get("/", (_, res) => res.send("Welcome to Eazika API v2"));
 app.get("/health", (_, res) => res.json({ status: "ok" }));
 
-const options = { explorer: true };
-
+import { swaggerHost, swaggerServeFile, swaggerSetup } from "./swagger.js";
 if (isNodeEnvDevelopment) {
-  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec, options));
+  app.use("/api-docs", swaggerHost, swaggerServeFile(), swaggerSetup());
 }
 
-// Routing
+// Routing;
 import routes from "./routes/index.js";
 app.use("/api/v2", routes);
 
--- src/swagger.ts
+++ src/swagger.ts
@@ -1,11 +1,34 @@
+import swaggerUi, { SwaggerOptions } from "swagger-ui-express";
 import path from "path";
-import swaggerUi from "swagger-ui-express";
 import fs from "fs";
+import { asyncHandler } from "./utils/asyncHandler";
 
-const file = fs.readFileSync(
-  path.resolve(__dirname, "../../api-docs.json"),
-  "utf8"
+const swaggerDocument = JSON.parse(
+  fs.readFileSync(path.resolve(__dirname, "../../api-docs.json"), "utf8")
 );
 
-export const swaggerSpec = JSON.parse(file);
-export { swaggerUi };
+const options: SwaggerOptions = {
+  swaggerOptions: {
+    docExpansion: "none",
+    filter: true,
+    showRequestHeaders: true,
+  },
+};
+
+declare global {
+  namespace Express {
+    interface Request {
+      swaggerDoc?: any;
+    }
+  }
+}
+const swaggerHost = asyncHandler(async (req, _, next) => {
+  swaggerDocument.host = req.get("host");
+  req.swaggerDoc = swaggerDocument;
+  next();
+});
+
+const swaggerServeFile = () => swaggerUi.serveFiles(swaggerDocument, options);
+const swaggerSetup = () => swaggerUi.setup(options);
+
+export { swaggerHost, swaggerServeFile, swaggerSetup };
